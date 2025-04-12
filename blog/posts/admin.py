c3fed7341259328e2bf0b09a6cb279b061b1f071
sierocka <izasiewaw@gmail.com>
2025-04-12 15:01:07 +0200

    4 modele cacy + toolbar

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # modele musimy zaimportować
 from .models import Category, Location, Employee, Device
 
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ["created_at", "modified_at"]
+
 # a następnie zarejestrować (pokazano najprostszy przypadek)
 admin.site.register(Category)
 admin.site.register(Location)
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    
 
 class Location(models.Model):
     name = models.CharField(max_length=100)
     floor = models.IntegerField(null=True, blank=True)
+
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=50)
@@ -20,4 +22,5 @@ class Device(models.Model):
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
     assigned_to = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True)
     purchase_date = models.DateField()
+
 
