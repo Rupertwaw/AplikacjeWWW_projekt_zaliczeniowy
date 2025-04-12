4df3e0c5d4aa9b01950bb92ed2d389efea1b201e
sierocka <izasiewaw@gmail.com>
2025-04-12 14:09:06 +0200

    4 modele

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # modele musimy zaimportować
-from .models import Category, Topic
+from .models import Category, Location, Employee, Device
 
 # a następnie zarejestrować (pokazano najprostszy przypadek)
 admin.site.register(Category)
-admin.site.register(Topic)
+admin.site.register(Location)
+admin.site.register(Employee)
+admin.site.register(Device)
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -2,11 +2,22 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=100)
 
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    floor = models.IntegerField(null=True, blank=True)
 
-class Topic(models.Model):
-    name = models.CharField(max_length=60)
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+   
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=100, unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+    assigned_to = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True)
+    purchase_date = models.DateField()
 
