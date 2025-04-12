837818b78a40cbe9544a67f763df62111827984e
sierocka <izasiewaw@gmail.com>
2025-04-12 17:32:58 +0200

    Dodanie urls.py

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,1 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+
+def welcome_view(request):
+    now = datetime.datetime.now()
+    html = f"""
+        <html><body>
+        Witaj użytkowniku! </br>
+        Aktualna data i czas na serwerze: {now}.
+        </body></html>"""
+    return HttpResponse(html)
