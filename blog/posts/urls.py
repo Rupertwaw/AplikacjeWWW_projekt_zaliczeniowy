fa91db83711b0b5b4a7257ae56da3c113b347140
sierocka <izasiewaw@gmail.com>
2025-04-12 18:50:01 +0200

    Dodanie views i forms

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-# importujemy moduł views (plik views.py z tego samego katalogu co plik bieżący)
 from . import views
 
-# definiujemy zmienną urlpatterns, która jest listą mapowań adresów URL na nasze widoki
+
 urlpatterns = [
     path("welcome", views.welcome_view),
+    path('categories/', views.category_list, name='category_list'),
+    path('locations/', views.location_list, name='location_list'),
+    path('employees/', views.employee_list, name='employee_list'),
+    path('devices/', views.device_list, name='device_list'),
+    path('devices/<int:pk>/', views.device_detail, name='device_detail'),
+    path('devices/<int:pk>/edit/', views.device_edit, name='device_edit'),
+    path('devices/<int:pk>/delete/', views.device_delete, name='device_delete'),
+    path('devices/category/<int:category_id>/', views.devices_by_category, name='devices_by_category'),
+    path('devices/employee/<int:employee_id>/', views.devices_by_employee, name='devices_by_employee'),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Category, Location, Employee, Device
+from .forms import DeviceForm
+from django.db.models import Count
+from datetime import date
 import datetime
+from django.http import HttpResponse
 
 
 def welcome_view(request):
@@ -11,3 +15,60 @@ def welcome_view(request):
         Aktualna data i czas na serwerze: {now}.
         </body></html>"""
     return HttpResponse(html)
+
+
+# Widoki listy
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'category_list.html', {'categories': categories})
+
+def location_list(request):
+    locations = Location.objects.all()
+    return render(request, 'location_list.html', {'locations': locations})
+
+def employee_list(request):
+    employees = Employee.objects.all()
+    return render(request, 'employee_list.html', {'employees': employees})
+
+def device_list(request):
+    devices = Device.objects.all()
+    return render(request, 'device_list.html', {'devices': devices})
+
+# Szczegóły jednego urządzenia
+def device_detail(request, pk):
+    device = get_object_or_404(Device, pk=pk)
+    return render(request, 'device_detail.html', {'device': device})
+
+# Edycja urządzenia
+def device_edit(request, pk):
+    device = get_object_or_404(Device, pk=pk)
+    if request.method == "POST":
+        form = DeviceForm(request.POST, instance=device)
+        if form.is_valid():
+            form.save()
+            return redirect('device_detail', pk=device.pk)
+    else:
+        form = DeviceForm(instance=device)
+    return render(request, 'device_edit.html', {'form': form})
+
+# Usunięcie urządzenia
+def device_delete(request, pk):
+    device = get_object_or_404(Device, pk=pk)
+    if request.method == "POST":
+        device.delete()
+        return redirect('device_list')
+    return render(request, 'device_delete.html', {'device': device})
+
+# Widok dodatkowy: lista urządzeń w danej kategorii
+def devices_by_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    devices = Device.objects.filter(category=category)
+    return render(request, 'devices_by_category.html', {'devices': devices, 'category': category})
+
+# Widok dodatkowy: urządzenia przypisane do danego pracownika
+def devices_by_employee(request, employee_id):
+    employee = get_object_or_404(Employee, id=employee_id)
+    devices = Device.objects.filter(assigned_to=employee)
+    return render(request, 'devices_by_employee.html', {'devices': devices, 'employee': employee})
+
+
